6df56ab3543f428770c679c045622c376c6c2481
Gesiel Neves <neves.gesiel@gmail.com>
2023-02-18 13:53:14 -0300

    Project created with some exercises

---
+++ Módulo 1/Semana 2/[M1S02] Ex 3 - Salvar login no LocalStorage [App LoginStart]/function.js
@@ -0,0 +1,42 @@
+const user = {
+  username: "",
+  password: "",
+};
+const usuarioCorreto = {
+  usuario: "admin",
+  senha: "1234",
+};
+
+const username = document.getElementById("username");
+const password = document.getElementById("password");
+
+const handleChangeUserName = (e) => {
+  user.username = e.target.value;
+};
+const handleChangePassword = (e) => {
+  user.password = e.target.value;
+};
+
+username.addEventListener("change", handleChangeUserName);
+password.addEventListener("change", handleChangePassword);
+
+const form = document.querySelector("#form");
+
+form.addEventListener("submit", (e) => {
+  e.preventDefault();
+  clicou();
+});
+
+function clicou() {
+  const db = JSON.parse(localStorage.getItem("dados"));
+
+  if (user.username === dados.usuario && user.password === dados.senha) {
+    location.href = "./login.html";
+  } else {
+    alert("Credenciais Incorretas!");
+  }
+}
+
+window.onload = () => {
+  localStorage.setItem("dados", JSON.stringify(usuarioCorreto));
+};
---
+++ Módulo 1/Semana 2/[M1S02] Ex2 - Criar login [App LoginStart]/function.js
@@ -0,0 +1,30 @@
+var user = {
+  username: "",
+  password: "",
+};
+var usuarioCorreto = {
+  username: "correto",
+  senha: "1234",
+};
+let login = document.getElementById("login");
+let username = document.getElementById("username");
+let password = document.getElementById("password");
+
+const handleChangeUsername = (event) => {
+  user.username = event.target.value;
+};
+const handleChangePassword = (event) => {
+  user.password = event.target.value;
+};
+
+login.addEventListener("click", clicou);
+username.addEventListener("change", handleChangeUsername);
+password.addEventListener("change", handleChangePassword);
+
+function clicou() {
+  if (user.username === usuarioCorreto.username) {
+    alert("Logado");
+  } else {
+    alert("Credenciais Incorretas!");
+  }
+}
