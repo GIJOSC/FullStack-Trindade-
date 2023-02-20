f0b8144687e007869eaa92d3db45740ab9daa600
Gesiel Neves <neves.gesiel@gmail.com>
2023-02-20 18:27:58 -0300

    feat: Cronometro

--- Módulo 1/Semana 2/[M1S02] Ex 5 - Cronômetro/script.js
+++ Módulo 1/Semana 2/[M1S02] Ex 5 - Cronômetro/script.js
@@ -1,78 +1,17 @@
-const minutesEl = document.querySelector("#minutes");
-const secondsEl = document.querySelector("#seconds");
-const millisecondsEl = document.querySelector("#milliseconds");
-const startBtn = document.querySelector("#startBtn");
-const pauseBtn = document.querySelector("#pauseBtn");
-const resumeBtn = document.querySelector("#resumeBtn");
-const resetBtn = document.querySelector("#resetBtn");
+document
+  .getElementById("cronometro")
+  .addEventListener("click", (cronometro) => {
+    let timer = document.getElementById("timer");
+    let i = 0;
 
-let interval;
-let minutes = 0;
-let seconds = 0;
-let milliseconds = 0;
-let isPaused = false;
-
-startBtn.addEventListener("click", startTimer);
-pauseBtn.addEventListener("click", pauseTimer);
-resumeBtn.addEventListener("click", resumeTimer);
-resetBtn.addEventListener("click", resetTimer);
-
-function startTimer() {
-  interval = setInterval(() => {
-    if (!isPaused) {
-      milliseconds += 10;
-
-      if (milliseconds === 1000) {
-        seconds++;
-        milliseconds = 0;
+    function startCronometro() {
+      if (i <= 10) {
+        timer.innerText = i++;
+      } else {
+        alert("Seu tempo acabou!! Tente novamente!!");
+        clearInterval(cronometro);
       }
-
-      if (seconds === 60) {
-        minutes++;
-        seconds = 0;
-      }
-
-      minutesEl.textContent = formatTime(minutes);
-      secondsEl.textContent = formatTime(seconds);
-      millisecondsEl.textContent = formatMilliseconds(milliseconds);
     }
-  }, 10);
 
-  startBtn.style.display = "none";
-  pauseBtn.style.display = "block";
-}
-
-function pauseTimer() {
-  isPaused = true;
-  pauseBtn.style.display = "none";
-  resumeBtn.style.display = "block";
-}
-
-function resumeTimer() {
-  isPaused = false;
-  pauseBtn.style.display = "block";
-  resumeBtn.style.display = "none";
-}
-
-function resetTimer() {
-  clearInterval(interval);
-  minutes = 0;
-  seconds = 0;
-  milliseconds = 0;
-
-  minutesEl.textContent = "00";
-  secondsEl.textContent = "00";
-  millisecondsEl.textContent = "000";
-
-  startBtn.style.display = "block";
-  pauseBtn.style.display = "none";
-  resumeBtn.style.display = "none";
-}
-
-function formatTime(time) {
-  return time < 10 ? `0${time}` : time;
-}
-
-function formatMilliseconds(time) {
-  return time < 100 ? `${time}`.padStart(3, "0") : time;
-}
+    var cronometro = setInterval(startCronometro, 1000);
+  });
