870fe38c81985e4465aaf7905dc47e3d22a97df1
Gesiel Neves <neves.gesiel@gmail.com>
2023-04-28 22:53:23 -0300

    [M1S06] Ex 8

---
+++ Módulo 1/Semana 6/[M1S06] Ex 8 - Criando uma API (parte 3)/userController.js
@@ -0,0 +1,51 @@
+const createUser = (req, res) => {
+  const { nome, idade, cargo, senha } = req.body;
+
+  if (!nome || !idade || !cargo || !senha) {
+    return res
+      .status(406)
+      .json({ mensagem: "Está faltando dados para concluir a operação" });
+  }
+
+  if (idade < 21) {
+    return res
+      .status(400)
+      .json({ mensagem: "Usuário não possui idade suficiente" });
+  }
+
+  // código para criar um novo usuário no banco de dados
+
+  return res.status(200).json({ mensagem: "Criado com sucesso" });
+};
+
+const deleteUser = (req, res) => {
+  const id = req.params.id;
+
+  if (!id) {
+    return res
+      .status(406)
+      .json({ mensagem: "Está faltando dados para concluir a operação" });
+  }
+
+  // código para deletar o usuário com o ID especificado do banco de dados
+
+  return res.status(200).json({ mensagem: "Deletado com sucesso" });
+};
+
+function verificarCargo(req, res, next) {
+  const { cargo } = req.body;
+
+  if (cargo === "líder") {
+    next();
+  } else {
+    return res
+      .status(403)
+      .json({
+        mensagem: "Usuário não tem permissão para acessar essa funcionalidade",
+      });
+  }
+}
+
+app.post("/usuarios", verificarCargo, UserController.createUser);
+
+module.exports = { createUser, deleteUser };
