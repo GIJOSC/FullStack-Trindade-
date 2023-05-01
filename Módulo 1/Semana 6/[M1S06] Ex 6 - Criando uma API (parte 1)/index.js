e94269ae512470fbf1e6f1109be71446a6fac5ec
Gesiel Neves <neves.gesiel@gmail.com>
2023-05-01 13:56:54 -0300

    fixed: [M1S06] Ex 6 - Criando uma API (parte 1)

--- Módulo 1/Semana 6/[M1S06] Ex 6 - Criando uma API (parte 1)/index.js
+++ Módulo 1/Semana 6/[M1S06] Ex 6 - Criando uma API (parte 1)/index.js
@@ -1,41 +1,18 @@
-require("dotenv").config();
 const express = require("express");
 
-const connection = require("./src/database");
-
-const log = require("./src/middlewares/log");
-const hello = require("./src/middlewares/hello");
-const validateNewUser = require("./src/middlewares/validate-new-user");
-const validateToken = require("./src/middlewares/validate-token");
-
-const createTask = require("./src/controllers/tasks/createTask");
-const findTasks = require("./src/controllers/tasks/findTasks");
-const deleteTask = require("./src/controllers/tasks/deleteTask");
-const updateTask = require("./src/controllers/tasks/updateTask");
-
-const createUser = require("./src/controllers/users/createUser");
-const createLogin = require("./src/controllers/users/createLogin");
-
 const app = express();
-app.use(express.json()); //obrigatório
 
-app.use(hello);
-app.use(log);
+const port = 5554;
 
-connection.authenticate();
-connection.sync({ alter: true });
-console.log("Connection has been established successfully.");
-
-app.get("/", (request, response) => {
-  response.json({ messagem: "Bem vindo" });
+app.get("/", (req, res) => {
+  res.send("Hello World!");
 });
 
-app.post("/tarefas", validateToken, createTask);
-app.get("/tarefas", validateToken, findTasks);
-app.delete("/tarefas/:id", validateToken, deleteTask);
-app.put("/tarefas/:id", validateToken, updateTask);
+app.post("/tarefas", (request, response) => {
+  console.log(request.body);
+  response.json({ messagem: "tudo bem" });
+});
 
-app.post("/users", validateNewUser, createUser);
-app.post("/users/login", createLogin);
-
-app.listen(4444, () => console.log("Aplicação online"));
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`);
+});
--- Módulo 1/Semana 6/[M1S06] Ex 6 - Criando uma API (parte 1)/src/middlewares/hello.js
+++
@@ -1,7 +0,0 @@
-function hello(request, response, next) {
-    console.log("Bem vindo")
-    
-    next()
-}
-
-module.exports = hello
